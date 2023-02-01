be70f8f370656c46718939642476872df2aa6978
JJune <iwhy7979@naver.com>
2023-02-01 11:22:26 +0900

    [ADD] 서버단 리뷰 수정,조회,삭제 api 및 로직 추가

--- src/main/java/com/example/matzip/controller/ReviewCtrl.java
+++ src/main/java/com/example/matzip/controller/ReviewCtrl.java
@@ -3,13 +3,13 @@ package com.example.matzip.controller;
 
 import com.example.matzip.dto.ReviewDTO;
 import com.example.matzip.dao.ReviewDAO;
+import lombok.extern.slf4j.XSlf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
+
 
 @RestController
 @RequestMapping("/api/review")
@@ -19,8 +19,24 @@ public class ReviewCtrl {
 
     @PostMapping("/saveReview")
     public void saveReview(@RequestBody ReviewDTO reviewDTO) {
-        String id = UUID.randomUUID().toString();
-        reviewDTO.setId(id);
+        String id = reviewDTO.getId();
+        System.out.println(id);
+        if (id == null) {
+            UUID uuid = UUID.randomUUID();
+            String uuidStr = uuid.toString();
+            System.out.println(uuid);
+            reviewDTO.setId(uuidStr);
+        }
         reviewDAO.saveReview(reviewDTO);
+    }
+
+    @GetMapping("/getReviews")
+    public List<ReviewDTO> getReviews() {
+        return reviewDAO.getReviews();
+    }
+
+    @DeleteMapping("/deleteReview")
+    public void deleteReview(@RequestParam(value = "id", required = true) String id){
+        reviewDAO.deleteReview(id);
     }
 }
--- src/main/java/com/example/matzip/dao/ReviewDAO.java
+++ src/main/java/com/example/matzip/dao/ReviewDAO.java
@@ -1,7 +1,14 @@
 package com.example.matzip.dao;
 
 import com.example.matzip.dto.ReviewDTO;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface ReviewDAO {
     void saveReview(ReviewDTO reviewDTO);
+
+    List<ReviewDTO> getReviews();
+
+    void deleteReview(@Param("id") String id);
 }
